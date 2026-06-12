302a279056246a11eb11906e66170a387d1bf114
anr1996 <anrich250@gmail.com>
2026-06-11 19:10:30 -0700

    Add User entity and Role enum

---
+++ backend/src/main/java/com/richenterprises/banking_api/entity/Role.java
@@ -0,0 +1,11 @@
+package com.richenterprises.banking_api.entity;
+
+/**
+ * This defines user roles for role-based access control (RBAC).
+ * The roles are stored as strings for readability and preventing bugs from reordering or 
+ * updating of roles within the enum.
+ */
+public enum Role {
+    CUSTOMER, ADMIN
+
+}
---
+++ backend/src/main/java/com/richenterprises/banking_api/entity/User.java
@@ -0,0 +1,62 @@
+package com.richenterprises.banking_api.entity;
+
+// JPA (Hibernate imports)
+import jakarta.persistence.*;
+
+// Lombok imports
+import lombok.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+// Java Time import
+import java.time.LocalDateTime;
+
+
+/**
+ * user entity for authentication and role-based access control.
+ * This will map to the users table in PostgreSQL.
+ */
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    /**
+     * Primary key.
+     * This will use IDENTITY to leverage PostgreSQL's BIGSERIAL type which 
+     * automatically generates a sequential, unique primary key value for the table.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* A unique identifier for login. It is enforced at the database level. */
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    /* Hash encrypted password */
+    @Column(nullable = false)
+    private String passwordHash;
+
+    /*Display name for the user */
+    private String name;
+
+    /*The role which determines access level: CUSTOMER or ADMIN.
+    * The role is stored as a STRING rather than an ORDINAL, avoiding the corruption of
+    prexisting data when the enum is updated.
+    */
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    /* A Timestamp that is set automatically upon creation.
+     * It is never updated after it is inserted.
+    */
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}

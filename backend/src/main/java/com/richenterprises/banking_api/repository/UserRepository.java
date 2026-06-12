4774e24be1055a98736fa883cc549fdabb2ebd13
anr1996 <anrich250@gmail.com>
2026-06-11 21:13:31 -0700

    add UserRepository with email lookup

---
+++ backend/src/main/java/com/richenterprises/banking_api/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.richenterprises.banking_api.repository;
+
+import com.richenterprises.banking_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
+    
+}
